ea07a34d33325b4b1cff9228d4c0b9e7a63622a7
Magnus Schulte-Tigges <magnus.schulte-tigges@tum.de>
2021-01-14 17:11:50 +0100

    Adding Explainaitions for the subsets

--- main.R
+++ main.R
@@ -5,8 +5,10 @@ payments <- read.csv("payments.csv")
 physicians <- read.csv("physicians.csv")
 previously_released_payments <- read.csv("previously_released_payments.csv")
 
-train <- physicians[physicians$set == 'train', ]
-test <- physicians[physicians$set == 'test', ]
+#A subset containig only Physicians Data that should be used for training
+trainPhysicians <- physicians[physicians$set == 'train', ]
+#A subset containig only Physicians Data that should be used for testing
+testPhysicians <- physicians[physicians$set == 'test', ]
 
 train_pay <- merge(x = train, y = payments, by.x = "id", by.y = "Physician_ID", all=TRUE)
 
@@ -15,12 +17,17 @@ summary(train)
 
 summary(train_pay)
 
+#A subset containing of only Payments Data that should be used for training
+paymentsTrain <- subset(payments, Physician_ID %in% trainPhysicians$id )
+#A subset containing of only Payments Data that should be used for testing
+paymentsTest <- subset(payments, Physician_ID %in% testPhysicians$id )
 
-paymentsTrain <- subset(payments, Physician_ID %in% train$id )
-paymentsTest <- subset(payments, Physician_ID %in% test$id )
-
+#A subset containing of only Payments Data that should be used for training AND the Ownership Indicator is YES
 paymentsTrainYes <- subset(paymentsTrain, Ownership_Indicator == 'Yes')
+#A subset containing of only Payments Data that should be used for training AND the Ownership Indicator is NO
 paymentsTrainNo <- subset(paymentsTrain, Ownership_Indicator == 'No')
 
+#A subset containing of only Physicians Data that should be used for training AND the Ownership Indicator is YES 
 trainPhysiciansYes <- subset(trainPhysicians, id %in% paymentsTrainYes$Physician_ID)
-trainPhysiciansNo <- subset(trainPhysicians, id %in% paymentsTrainYes$Physician_ID)
+#A subset containing of only Physicians Data that should be used for training AND the Ownership Indicator is NO -- PROBABLY NOT USABLE - NOT IMPORTANT
+trainPhysiciansNo <- subset(trainPhysicians, id %in% paymentsTrainNo$Physician_ID)
