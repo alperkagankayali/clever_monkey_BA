b969299959e8d7d25691c8ab334746a708fbc328
Magnus Schulte-Tigges <magnus.schulte-tigges@tum.de>
2021-01-20 01:36:19 +0100

    Analysed phyisician Data
    Speciality is maybe intresting, as there is a high likelyhood for some data that there will be a positive rating

--- main.R
+++ main.R
@@ -31,3 +31,42 @@ paymentsTrainNo <- subset(paymentsTrain, Ownership_Indicator == 'No')
 trainPhysiciansYes <- subset(trainPhysicians, id %in% paymentsTrainYes$Physician_ID)
 #A subset containing of only Physicians Data that should be used for training AND the Ownership Indicator is NO -- PROBABLY NOT USABLE - NOT IMPORTANT
 trainPhysiciansNo <- subset(trainPhysicians, id %in% paymentsTrainNo$Physician_ID)
+
+#Check if there is a significant difference in staates -> is not
+table(trainPhysiciansYes$State)/NROW(trainPhysiciansYes)
+table(trainPhysiciansNo$State)/NROW(trainPhysiciansNo)
+table(trainPhysicians$State)/NROW(trainPhysicians)
+
+
+#Check if there is a significant difference in cities -> is not
+table(trainPhysiciansYes$City)/NROW(trainPhysiciansYes)
+table(trainPhysiciansNo$City)/NROW(trainPhysiciansNo)
+table(trainPhysicians$City)/NROW(trainPhysicians)
+
+#Check Countries -> All out of the USA but only 0.02 from United States Minor Outlying Island. So not significant
+table(trainPhysiciansYes$Country)/NROW(trainPhysiciansYes)
+table(trainPhysiciansNo$Country)/NROW(trainPhysiciansNo)
+NROW(trainPhysiciansYes)
+
+#Check Primary Specialities Neurology -> Very likely its getting postive  23% (positive) vs 2.34% (negativ) if you are a Neurology Physisician there is a likelyhood of 95 % chance it is positive
+# Urology it is roughly 9 % vs 2.62 %
+# Ophthalmology 7.6 % vs 2.96
+table(trainPhysiciansYes$Primary_Specialty)
+
+table(trainPhysiciansNo$Primary_Specialty)
+count(trainPhysiciansYes,)
+setDT(trainPhysiciansYes)[,.N, by=Primary_Specialty]
+
+library(data.table)
+orderdPYPS <- setDT(trainPhysiciansYes)[, .N, by = Primary_Specialty][order(-N)]
+orderdPYPSFrequency <-orderdPYPS
+orderdPYPSFrequency$N <- as.numeric(orderdPYPS$N)/ NROW(trainPhysiciansYes)
+orderdPYPSFrequency
+
+orderdPNPS <- setDT(trainPhysiciansNo)[, .N, by = Primary_Specialty][order(-N)]
+orderdPNPSFrequency <-orderdPNPS
+orderdPNPSFrequency$N <- as.numeric(orderdPNPS$N)/ NROW(trainPhysiciansNo)
+orderdPNPSFrequency
+
+orderdPAPS <- setDT(trainPhysicians)[, .N, by = Primary_Specialty][order(-N)]
+
